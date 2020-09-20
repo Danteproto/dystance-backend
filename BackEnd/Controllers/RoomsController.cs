ae20422f344e5155274b30758d027e9fbf53f307
TruongAnhTu <tutahe130534@fpt.edu.vn>
2020-09-20 22:39:34 +0700

    DL-90: Change Show Rooms API properties to camelCase

--- BackEnd/Controllers/RoomsController.cs
+++ BackEnd/Controllers/RoomsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace BackEnd.Controllers
     [ApiController]
     public class RoomsController : ControllerBase
     {
+        private readonly DefaultContractResolver _contractResolver;
         private readonly RoomDBContext _context;
         private readonly IWebHostEnvironment _env;
 
@@ -24,6 +26,10 @@ namespace BackEnd.Controllers
         {
             _context = context;
             _env = env;
+            _contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            };
         }
 
         [HttpPost("create")]
@@ -41,20 +47,28 @@ namespace BackEnd.Controllers
         public IActionResult GetRoomById(int Id)
         {
             Room room = RoomService.GetRoomById(_context, Id);
-            return Content(JsonConvert.SerializeObject(room));
+            return Content(JsonConvert.SerializeObject(room, new JsonSerializerSettings
+            {
+                ContractResolver = _contractResolver,
+                Formatting = Formatting.Indented
+            }));
         }
 
         [HttpGet("getByUserId")]
         public IActionResult GetRoomByUserId(int Id)
         {
             var rooms = RoomService.GetRoomByUserId(_context, Id);
-            return Content(JsonConvert.SerializeObject(rooms));
+            return Content(JsonConvert.SerializeObject(rooms, new JsonSerializerSettings
+            {
+                ContractResolver = _contractResolver,
+                Formatting = Formatting.Indented
+            }));
         }
         [HttpGet("Get/Image/Room")]
         public async Task<IActionResult> GetImage(int roomId, string imgName)
         {
             var rootPath = _env.ContentRootPath;
-            var path = Path.Combine(rootPath, "Images/" + roomId);
+            var path = Path.Combine(rootPath, $"Images/{roomId}");
             var imgPath = Path.Combine(path, imgName);
             var image = System.IO.File.OpenRead(imgPath);
             return File(image, "image/jpeg");
