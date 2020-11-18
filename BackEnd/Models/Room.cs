5221f2792b91c06a7bbc850fd787b952069a1741
TruongAnhTu <tutahe130534@fpt.edu.vn>
2020-11-18 22:09:41 +0700

    fix null able field

--- BackEnd/Models/Room.cs
+++ BackEnd/Models/Room.cs
@@ -11,13 +11,13 @@ namespace BackEnd.Models
         [Key]
         public int RoomId { get; set; }
         public string Subject { get; set; }
-        public string ClassName { get; set; }
-        public string CreatorId { get; set; }
-        public string Image { get; set; }
+        public string? ClassName { get; set; }
+        public string? CreatorId { get; set; }
+        public string? Image { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public bool Group { get; set; }
         public int? MainRoomId { get; set; }
-        public int SemesterId { get; set; }
+        public int? SemesterId { get; set; }
     }
 }
