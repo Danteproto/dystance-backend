4f34593bb0bd3b5ca4ef288d5efebc3a46824a44
Kuro091 <lcminhhp@gmail.com>
2020-11-22 18:26:59 +0700

    Handling semesterId for teacher/student CRUD

--- BackEnd/Context/UserDbContext.cs
+++ BackEnd/Context/UserDbContext.cs
@@ -16,6 +16,8 @@ namespace BackEnd.Context
         public DbSet<RefreshToken> RefreshTokens { get; set; }
         public DbSet<PrivateMessage> PrivateMessages { get; set; }
         public DbSet<UsersLog> UserLog { get; set; }
+        public DbSet<UserSemesters> UserSemesters { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {            base.OnModelCreating(builder);
 
@@ -26,7 +28,6 @@ namespace BackEnd.Context
             builder.Entity<IdentityUserLogin<string>>(entity => { entity.ToTable("UserLogins"); });
             builder.Entity<IdentityUserToken<string>>(entity => { entity.ToTable("UserTokens"); });
             builder.Entity<IdentityRoleClaim<string>>(entity => { entity.ToTable("RoleClaims"); });
-
         }
     }
 }
---
+++ BackEnd/DAO/UserSemesterDAO.cs
@@ -0,0 +1,30 @@
+﻿using BackEnd.Context;
+using BackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.DAO
+{
+    public class UserSemesterDAO
+    {
+        public static async Task<int> Create(UserDbContext context, UserSemesters userSemester)
+        {
+            await context.UserSemesters.AddAsync(userSemester);
+            return await context.SaveChangesAsync();
+        }
+        public static async Task<int> Update(UserDbContext context, UserSemesters userSemester)
+        {
+            context.UserSemesters.Update(userSemester);
+            return await context.SaveChangesAsync();
+        }
+        public static async Task<int> Delete(UserDbContext context, UserSemesters userSemester)
+        {
+            context.UserSemesters.Remove(userSemester);
+            return await context.SaveChangesAsync();
+        }
+
+    }
+}
